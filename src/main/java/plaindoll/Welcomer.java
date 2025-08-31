910f4d561bf9069ec3323405a1e8a190e6c2e51a
GlubuchikAr <aeg6011@gmail.com>
2025-08-31 18:43:22 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String getHunterReply() {
+    return "The hunter becomes the hunted when facing true challenges";
+	}
 }
