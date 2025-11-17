ed63c2d13f96b66947a4a040a1782be9f14a9040
mlnstw <sammygsn@gmail.com>
2025-11-17 23:47:22 +0700

    change .kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,35 +1,28 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.maven
 
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
-
 version = "2025.07"
 
 project {
     description = "test_project_from_video"
 
+    vcsRoot(Netology_HttpsGithubComMlnstwExampleTeamcityZhukov_VcsRoot)
     buildType(Netology_HttpsGithubComMlnstwExampleTeamcityZhukov)
 }
+
+object Netology_HttpsGithubComMlnstwExampleTeamcityZhukov_VcsRoot : GitVcsRoot({
+    name = "https://github.com/mlnstw/example-teamcity-zhukov"
+    url = "https://github.com/mlnstw/example-teamcity-zhukov.git"
+    branch = "refs/heads/master"
+    branchSpec = """
+        +:refs/heads/master
+        +:refs/heads/feature/*
+    """.trimIndent()
+    authMethod = uploadedKey {
+        userName = "git"
+        uploadedKey = "TeamCity Deployment Key"
+    }
+})
 
 object Netology_HttpsGithubComMlnstwExampleTeamcityZhukov : BuildType({
     id = DslContext.settingsRoot.id
@@ -37,7 +30,7 @@ object Netology_HttpsGithubComMlnstwExampleTeamcityZhukov : BuildType({
     description = "example-teamcity-zhukov"
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(Netology_HttpsGithubComMlnstwExampleTeamcityZhukov_VcsRoot)
     }
 
     steps {
@@ -48,7 +41,8 @@ object Netology_HttpsGithubComMlnstwExampleTeamcityZhukov : BuildType({
             userSettingsSelection = "settings.xml"
         }
     }
-		triggers {
+
+    triggers {
         vcs {
             id = "VCS_TRIGGER"
             branchFilter = "+:*"
