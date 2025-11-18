f4ce225c5273ecb2d0c3ea200122250a969592fd
hawk0774 <149391365+hawk0774@users.noreply.github.com>
2025-11-18 20:49:45 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayHunter(){
+		return "The hunter does not know where the pheasant is";
+	}
 }
