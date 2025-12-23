cf9d9158437c93d5ac59feefba56387b59942f42
Damir Gainullin <reqroot.pro@gmail.com>
2025-12-23 09:57:38 +0000

    Bump version to 0.0.3

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,16 +1,20 @@
 package plaindoll;
 
 public class Welcomer{
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-	public String sayNeedGold(){
-		return "Not enough gold";
-	}
-	public String saySome(){
-		return "something in the way";
-	}
+    public String sayWelcome() {
+        return "Welcome home, good hunter. What is it your desire?";
+    }
+    public String sayFarewell() {
+        return "Farewell, good hunter. May you find your worth in waking world.";
+    }
+    public String sayNeedGold(){
+        return "Not enough gold";
+    }
+    public String saySome(){
+        return "something in the way";
+    }
+    // ДОБАВЬ ЭТУ СТРОКУ:
+    public String getHunterReply() {
+        return "I'm a hunter of bugs!";
+    }
 }
