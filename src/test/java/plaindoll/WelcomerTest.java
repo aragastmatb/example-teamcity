93bf3d99ce771aefb24b2f5d09fa82172b03fc21
Vladimir Inshakov <mr_gogh@mail.ru>
2026-02-20 06:17:24 +0400

    New test added

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	//My new test
+	@Test
+	public void welcomerSaysAJoke(){
+	    assertThat(welcomer.sayAJoke(), containsString("hunter"));
+	}
 }
