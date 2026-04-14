4e3a62c9b7f4d9a6bf3c90a2b470ec5de000620f
Anna Os <53312078+larisaso66@users.noreply.github.com>
2026-04-15 00:12:32 +0300

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void testHunterReply() {
+		assertTrue(welcomer.getHunterReply().contains("hunter"));
+	}
 }
