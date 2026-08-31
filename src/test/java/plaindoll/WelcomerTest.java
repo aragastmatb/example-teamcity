b1719fa101f02b3a472d2204401eae2bc34ff6f1
Briefklammern <peopleforever2007@gmail.com>
2026-08-31 15:38:42 +0500

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -23,6 +23,7 @@ public class WelcomerTest {
 	public void welcomerSaysHunter() {
 		assertThat(welcomer.sayWelcome(), containsString("hunter"));
 		assertThat(welcomer.sayFarewell(), containsString("hunter"));
+		assertThat(welcomer.saySomething(), containsString("hunter"));
 	}
 	@Test
 	public void welcomerSaysSilver(){
